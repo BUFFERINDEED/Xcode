7b014983aaf96b2e1a1889bbad783a0a0f9065a2
Stori <wuyongs52.re.led@gmail.com>
2019-11-08 20:09:54 +0800

    Add test .cpp file

---
+++ Test_Func/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Test_Func
+//
+//  Created by Reverse on 2019/11/8.
+//  Copyright © 2019 Ayaya. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+//cstring str = "~/Downloads/photo_2019-10-30 05.35.05.jpeg";
+string str = "~/Downloads/photo_2019-10-30 05.35.05.jpeg";
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    //system("ls ~/Downloads/*.jpeg");
+    system("file ~/Downloads/photo_2019-10-30\\ 05.35.05.jpeg");
+    //std::cout << i << endl;
+    return 0;
+}
